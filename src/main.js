954ded9a9b01a90180421d8643e909d94d172c48
Alicca-miao <yusangmuran@163.com>
2024-06-12 20:07:50 +0800

    commit input component

--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import button from './components/button.vue'
 import dialogue from './components/dialogue.vue'
+import input from './components/input.vue'
+import miaoSwitch from './components/switch.vue'
 import './assets/fonts/iconfont.css'
 Vue.config.productionTip = false
 Vue.component(button.name,button)
 Vue.component(dialogue.name,dialogue)
+Vue.component(input.name,input)
+Vue.component(miaoSwitch.name,miaoSwitch)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
